a87675b85ae599207d7fcc2d9da478f840d922fe
Huertz <104065687+Huertz@users.noreply.github.com>
2023-01-11 01:49:08 -0600

    modified schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+roles_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT,
+FOREIGN KEY (department_id)
+REFERENCES departments(id)
+ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT DEFAULT NULL,
+FOREIGN KEY (role_id) REFERENCES roles(roles_id),
+FOREIGN KEY (manager_id) REFERENCES employees(employee_id),
+);
