203132fed03de17af4e04d5c7eb6156cd7df603c
Huertz <104065687+Huertz@users.noreply.github.com>
2023-01-10 23:12:34 -0600

    modified connection.js/changed file type

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Huerta95',
-  database: 'company_db',
-});
+const connection = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Huerta95',
+    database: 'company_db',
+  },
+  console.log(`Connected to the company_db database.`)
+);
 
 module.exports = connection;
