dfa59149954d4f8c2f74f13c2d100271057c02c5
Huertz <104065687+Huertz@users.noreply.github.com>
2023-01-11 02:05:23 -0600

    added values of roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,13 @@ VALUES ('Engeneering');
        ('Finance');
        ('Legal');
        ('Sales');
+
+INSERT INTO roles (title, department_id, salary)
+VALUES ('Lead Sales', 004, 100000),
+       ('Salesperson', 004, 80000),
+       ('Lead Engineer', 001, 150000),
+       ('Software Engineer', 001, 120000),
+       ('Account Manager', 002, 160000),
+       ('Accountant', 002, 125000),
+       ('Legal Team Lead', 003, 250000),
+       ('Lawyer', 003, 190000);
