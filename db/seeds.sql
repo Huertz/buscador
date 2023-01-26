63cab9e6fe2905681e27d49f37a6afec9216a632
Huertz <104065687+Huertz@users.noreply.github.com>
2023-01-26 02:50:01 -0600

    added seed for role

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,8 @@ VALUES ("Sales"),
        ("Engineering"),
        ("Finance"),
        ("Legal");
+
+// role seed
+INSERT INTO role (title, salary department)
+VALUES ("Lead Engineer", 150000, 2);
+       ("Legal Team Lead", 250000, 4);
