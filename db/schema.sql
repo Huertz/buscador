387d8c5ff135ac716c61ef80da9695dc0c362e8a
Huertz <104065687+Huertz@users.noreply.github.com>
2023-01-26 02:22:36 -0600

    created employee_trackerdb in schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+-- db created
+DROP DATABASE IF EXISTS employee_trackerdb;
+CREATE DATABASE employee_trackerdb;
+
+USE employee_trackerdb;
+
+--  created department 
+
+-- created role
+
+-- created employee
