d9a8c263aa83fc328aa3be930a1a6024d688a7eb
Huertz <104065687+Huertz@users.noreply.github.com>
2023-01-26 02:38:07 -0600

    schema.sql added department, role, employee

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,26 @@ CREATE DATABASE employee_trackerdb;
 
 USE employee_trackerdb;
 
---  created department 
+--  created department table
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
 
--- created role
+-- created role table
+CREATE TABLE role(
+    id INT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
 
--- created employee
+-- created employee table 
+CREATE TABLE employee(
+    id INT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    manager_id INT, 
+    role_id INT,
+)
