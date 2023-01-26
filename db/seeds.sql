115d1d6b6b31cbdd4cee47cdbd4848c80d2e8298
Huertz <104065687+Huertz@users.noreply.github.com>
2023-01-26 03:29:29 -0600

    modified seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,6 +9,11 @@ VALUES ("Sales"),
 INSERT INTO role (title, salary department)
 VALUES ("Lead Engineer", 150000, 2);
        ("Legal Team Lead", 250000, 4);
+       ("Accountant", 125000, 3);
+       ("Sales Lead", 100000, 1);
+       ("Salesperson", 80000, 1);
+       ("Software Engineer", 120000, 2);
+       ("Lawyer", 190000, 4);
 
 -- employee seed
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
