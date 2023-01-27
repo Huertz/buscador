b2377fd607cb2e3d582cd2b60a39f516db474c61
Huertz <104065687+Huertz@users.noreply.github.com>
2023-01-26 19:39:48 -0600

    inserted each value individually

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,41 @@
 -- department seed
-INSERT INTO department(name)
-VALUES ("Sales"),
-       ("Engineering"),
-       ("Finance"),
-       ("Legal");
+INSERT INTO department (name)
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Legal");
 
---  role seed
-INSERT INTO role (title, salary department)
-VALUES ("Lead Engineer", 150000, 2);
-       ("Legal Team Lead", 250000, 4);
-       ("Accountant", 125000, 3);
-       ("Sales Lead", 100000, 1);
-       ("Salesperson", 80000, 1);
-       ("Software Engineer", 120000, 2);
-       ("Lawyer", 190000, 4);
+-- role seed
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Legal Team Lead", 250000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Salesperson", 80000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 190000, 4);
 
--- employee seed
+--  employee seed
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUES ("Lionel", "Messi", null, 1);
-       ("Cristiano", "Ronaldo", null, 2);
-       ("Sergio", "Ramos", null, 3);
-       ("Andres", "Iniesta", 1, 4);
-       ("Andriy", "Shevchenko", 4, 5);
-       ("Antoine", "Griezmann", 1, 6);
-       ("Eden", "Hazard", 2, 7);
+VALUE ("Lionel", "Messi", null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Cristiano", "Ronaldo", null, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Sergio","Ramos",null,3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Andres", "Iniesta", 1, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Andriy", "Shevchenko", 4, 5);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Antoine", "Griezmann", 1, 6);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Eden", "Hazard", 2, 7);
